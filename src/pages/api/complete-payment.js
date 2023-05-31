ae6cfa0b49e9aee31966f6f70cc0005d67c7c7c4
kryptonumstudio <kryptonumstudio@gmail.com>
2023-05-31 18:23:03 +0200

    fix complete payment api

--- src/pages/api/complete-payment.js
+++ src/pages/api/complete-payment.js
@@ -9,12 +9,14 @@ const api = new WooCommerceRestApi({
 
 export default async function handler(req, res) {
     let status = req.query.redirect_status === 'succeeded' ? 'processing' : 'cancelled'
+
+    // ?id=528&redirect_status=succeeded&payment_intent=pi_3NDowYI9MWE3PvcY0TE4MiGt&payment_intent_client_secret=pi_3NDowYI9MWE3PvcY0TE4MiGt_secret_hrrVOUdPQ8EqkVrJBfX55cOkB
     api.put(`orders/${req.query.id}`, {
         status: status,
         transaction_id: req.query.payment_intent
+    }).then(response => {
+        res.status(201).redirect(`/podziekowanie?status=${req.query.redirect_status}&order=${req.query.id}`)
     }).catch(err => {
         res.status(500).send('Błąd podczas aktualizacji zamówienia, napisz do nas, twój numer zamówienia to: ' + req.query.id + ', twój numer płatności to: ' + req.query.payment_intent);
-    }).then(res => {
-        res.status(201).redirect(`/podziekowanie?status=${req.query.redirect_status}&order=${req.query.id}`)
     })
 };
